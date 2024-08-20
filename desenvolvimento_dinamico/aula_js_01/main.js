847d6d8892590771d36d73e870f1b146dab4cf0b
Aryman Reis <arymanreis@gmail.com>
2024-08-20 20:16:57 -0300

    Avançando na faculdade

--- desenvolvimento_dinamico/aula_js_01/main.js
+++ desenvolvimento_dinamico/aula_js_01/main.js
@@ -7,3 +7,15 @@ let numberTwo = 20;
 
 let sum = numberOne + numberTwo;
 console.log(sum);
+
+let n = 20;
+const name = 23;
+console.log(typeof(n));
+
+if(typeof(name) == "string") {
+  console.log(name.toUpperCase());
+} else {
+  console.log("Não é uma string")
+};
+
+
---
+++ desenvolvimento_dinamico/aula_js_01/operador.js
@@ -0,0 +1,23 @@
+let n = 3;
+//n = n * 10;
+n *= 10;
+
+console.log(n);
+
+let x = 10;
+x++
+
+console.log(x);
+
+console.log(n < x);
+console.log(n != x);
+console.log(n <= x);
+
+b = 20;
+c = 30;
+
+console.log(n <= x && b != c);
+
+console.log(c < b && b % 2 == 0);
+
+console.log( c <= b || b / 2 == 2);
