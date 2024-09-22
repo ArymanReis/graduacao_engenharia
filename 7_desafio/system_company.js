aabf33c8ce6438d966f9ba138f855d6582235bca
Aryman Reis <arymanreis@gmail.com>
2024-09-22 12:37:05 -0300

    Avançando em desenvolvimento dinâmico

--- 7_desafio/system_company.js
+++ 7_desafio/system_company.js
@@ -1,8 +1,6 @@
-const button = document.querySelector("#send"); 
-
+const button = document.querySelector("#send");
 
 class Employee {
-
   constructor(name, age, position) {
     this.name = name;
     this.age = age;
@@ -19,19 +17,18 @@ class Employee {
 }
 
 class Manager extends Employee {
-
-  constructor(name, age, position, department) {
+  constructor(name, age, position, department, linguage) {
     super(name, age, position);
     this.department = department;
+    this.linguage = linguage;
   }
 
   manage() {
-    console.log(`Meu nome é: ${this.name}, Tenho: ${this.age} anos, sou: ${this.position} do departamento de ${this.department}`);
+    return `Meu nome é: ${this.name}, Tenho: ${this.age} anos, sou: ${this.position} do departamento de ${this.department} e uso a linguagem ${this.linguage}`;
   }
 }
 
 class Developer extends Employee {
-
   constructor(name, age, position, linguage) {
     super(name, age, position);
     this.linguage = linguage;
@@ -42,27 +39,35 @@ class Developer extends Employee {
   }
 }
 
+function exibirErro(userName, ageUser, positionUser, departmentUser, linguageProg) {
+  if (!userName || !ageUser || !positionUser || !departmentUser || !linguageProg) {
+    throw new Error("Todos os campos são obrigatórios.");
+  }
+}
 
-
-button.addEventListener("click" , (e) => {
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  
+
   const userName = document.querySelector("#employee").value;
   const ageUser = document.querySelector("#age").value;
   const positionUser = document.querySelector("#position").value;
   const departmentUser = document.querySelector("#department").value;
   const linguageProg = document.querySelector("#linguage").value;
 
-  const developerSenior = new Developer(userName, ageUser, positionUser, linguageProg);
-  const managerPerson = new Manager(userName, ageUser, positionUser, departmentUser, linguageProg);
+  try {    
+    exibirErro(userName, ageUser, positionUser, departmentUser, linguageProg);
 
-  developerSenior.program();
-  managerPerson.manage();
+    const managerPerson = new Manager(userName, ageUser, positionUser, departmentUser, linguageProg);
+    const divSummary = document.querySelector("#summary");
+    const summary = document.createElement("p");
 
+    const descriptionMan = managerPerson.manage();
+    summary.innerHTML = descriptionMan;
+    divSummary.innerHTML = "";  // Limpa a div antes de adicionar a nova descrição
+    divSummary.append(summary);
+
+  } catch (error) {
+    console.log(`Ocorreu um erro: ${error.message}`);
+    alert(`Erro: ${error.message}`);
+  }
 });
-
-
-
-
-
-
